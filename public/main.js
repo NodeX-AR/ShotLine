f61847f221c33e2f036f28634da9f8664df2166a
ASWANTH R <aswanthr82@gmail.com>
2026-09-23 00:31:07 +0530

    Invert vector direction in ikFP function calls

--- public/main.js
+++ public/main.js
@@ -1094,7 +1094,7 @@ const VM=(function(){
         bn.rF.getWorldPosition(_fpV2);
         bn.rH.getWorldPosition(_fpV3);
         const l1=_fpV1.distanceTo(_fpV2), l2=_fpV2.distanceTo(_fpV3);
-        ikFP(_fpV1,wristLocal,l1,l2,new THREE.Vector3(0.55,-1,0.35),_fpV4,_fpV5);
+        ikFP(_fpV1,wristLocal,l1,l2,new THREE.Vector3(-0.55,-1,0.35),_fpV4,_fpV5);
         aimBone(bn.rA,bn.rF,_fpV4);
         aimBone(bn.rF,bn.rH,_fpV5);
         if(!reloading){
@@ -1124,7 +1124,7 @@ const VM=(function(){
         bn.lF.getWorldPosition(_fpV2);
         bn.lH.getWorldPosition(_fpV3);
         const l1=_fpV1.distanceTo(_fpV2), l2=_fpV2.distanceTo(_fpV3);
-        ikFP(_fpV1,wristLocal,l1,l2,new THREE.Vector3(-0.55,-1,0.35),_fpV4,_fpV5);
+        ikFP(_fpV1,wristLocal,l1,l2,new THREE.Vector3(0.55,-1,0.35),_fpV4,_fpV5);
         aimBone(bn.lA,bn.lF,_fpV4);
         aimBone(bn.lF,bn.lH,_fpV5);
         if(!reloading){
